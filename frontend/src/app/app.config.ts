e4fb89df9bc16b234939c77666984ec1a573e04a
renzoPeraza <renzoperaza16@gmail.com>
2026-05-03 21:25:33 -0300

    Home
    PrimeNg instalado
    PrimeNg Icons y animations instalados
    
    Pagina Home/votacion iniciada

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(), provideClientHydration(withEventReplay()),
-    
+    provideAnimations()
   ]
 };
+
+
--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
+    CardModule,
+    ToastModule
+  ],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrls: ['./home.scss']
 })
 export class Home {
 
+  document: string = '';
+  showCandidates: boolean = false;
+
+  candidates = [];
+
+  searchDocument() {
+    this.showCandidates = true;
+  }
+
+  vote(id: number) {
+    console.log(id);
+  }
 }
